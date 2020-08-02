3009ed1a027a3f56a6646afd55fdb72f12bbb2be
Shanaka <madushanka991@gmail.com>
2020-08-02 07:44:51 +0530

    UI fixes

--- src/app/modules/oauth/forgot-password/forgot-password.component.ts
+++ src/app/modules/oauth/forgot-password/forgot-password.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'c-inv-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.sass']
 })
 export class ForgotPasswordComponent implements OnInit {
 
