4d09402aa7f3595eae45bac995f0af2f739d9fb1
Shanaka <madushanka991@gmail.com>
2020-08-03 20:51:34 +0530

    Allow for FormControlName --> added a valueAccessor

--- src/app/modules/pos/unit-adder/unit-adder.component.ts
+++ src/app/modules/pos/unit-adder/unit-adder.component.ts
@@ -1,28 +1,50 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, forwardRef, Input, OnInit, Optional, Output} from '@angular/core';
+import {ControlContainer, ControlValueAccessor, FormControl, FormGroup, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'c-inv-unit-adder',
   templateUrl: './unit-adder.component.html',
-  styleUrls: ['./unit-adder.component.sass']
+  styleUrls: ['./unit-adder.component.sass'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => UnitAdderComponent),
+    multi: true
+    }
+  ]
 })
-export class UnitAdderComponent implements OnInit {
+export class UnitAdderComponent implements OnInit, ControlValueAccessor  {
 
-  @Output() value: EventEmitter<any> = new EventEmitter<any>();
-  count = 0;
+  @Input() count = 0;
+
+  propagateChange = (_: any) => {};
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   setCount(type) {
     switch (type) {
-      case 'ADD': this.count ++; break;
-      case 'REMOVE': this.count <= 0 ? this.count = 0 : this.count --; break;
+      case 'ADD': {
+        this.count++;
+        this.propagateChange(this.count);
+        break;
+      }
+      case 'REMOVE': {
+        this.count <= 0 ? this.count = 0 : this.count --;
+        this.propagateChange(this.count);
+        break;
+      }
     }
   }
 
-  getValue() {
-    return this.value.emit({});
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {}
+
+  writeValue(value: any): void {
+    this.count = value;
   }
 
 }
